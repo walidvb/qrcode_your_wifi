cb5250e7657f415980c4107ef2cde4c759777faa
Walid <hello@walidvb.com>
2021-08-25 19:23:56 +0200

    adds script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SSID_NAME=`/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}'`
+PASS=`security find-generic-password -wa $SSID_NAME`
+CODE="WIFI:S:$SSID_NAME;T:WPA;P:$PASS;;"
+echo $CODE | qrencode -o - -t UTF8
